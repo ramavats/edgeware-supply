45033a15a15382100eb4ed81c212b46f04addc32
Rama Shankar Jha <guy55222@gmail.com>
2023-07-27 17:41:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,34 @@
 #!/usr/bin/env node
 
-const { ApiPromise, WsProvider } = require('@polkadot/api');
-const { bnToBn } = require('@polkadot/util/bn');
-const { stringToU8a } = require('@polkadot/util');
-const { u128 } = require('@polkadot/types');
-const { Mainnet } = require('@edgeware/node-types');
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { bnToBn } from '@polkadot/util';
+import { u128 } from '@polkadot/types';
 
 module.exports = async (req, res) => {
-  const nodeUrl = 'ws://mainnet1.edgewa.re:9944';
+  const nodeUrl = 'wss://edgeware.jelliedowl.net';
 
   console.log(`Connecting to API for ${nodeUrl}...`);
-  let connected;
-  setTimeout(() => {
-    if (connected) return;
-    res.setHeader('content-type', 'text/plain');
-    res.status(500).send('Connection timed out');
-    process.exit(1);
-  }, 2000);
 
-  // initialize the api
-  const api = await ApiPromise.create({
-    provider: new WsProvider(nodeUrl),
-    ...Mainnet,
-  });
-  connected = true;
-
-  const TREASURY_ACCOUNT = stringToU8a('modlpy/trsry'.padEnd(32, '\0'));
-  //
-  // get relevant chain data
-  //
   try {
-    const [issuance, treasury, properties, block] = await Promise.all([
-      api.query.balances.totalIssuance(),
-      api.derive.balances.account(TREASURY_ACCOUNT),
-      api.rpc.system.properties(),
-    ]);
+    // Initialize the API
+    const api = await ApiPromise.create({
+      provider: new WsProvider(nodeUrl),
+    });
+
+    // Fetch the total issuance
+    const issuance = await api.query.balances.totalIssuance();
+    const properties = await api.rpc.system.properties();
+
+    // Calculate issuance in human-readable format
     const tokenDecimals = properties.tokenDecimals.unwrap().toString(10);
     const issuanceStr = issuance.div(bnToBn(10).pow(bnToBn(tokenDecimals))).toString(10);
-    const treasuryStr = treasury.freeBalance.div(bnToBn(10).pow(bnToBn(tokenDecimals))).toString(10);
-    const circulatingStr = issuance.sub(treasury.freeBalance).div(bnToBn(10).pow(bnToBn(tokenDecimals))).toString(10);
-    res.setHeader('content-type', 'text/plain');
 
-    if (!!req.query.circulating) {
-      res.status(200).send(circulatingStr);
-    } else if (!!req.query.treasury) {
-      res.status(200).send(treasuryStr);
-    } else {
-      res.status(200).send(issuanceStr);
-    }
-  } catch (e) {
+    // Return the total issuance
     res.setHeader('content-type', 'text/plain');
-    res.status(500).send('Error fetching Edgeware supply data');
+    res.status(200).send(issuanceStr);
+  } catch (e) {
+    console.error('Error fetching Edgeware total issuance:', e);
+    res.setHeader('content-type', 'text/plain');
+    res.status(500).send('Error fetching Edgeware total issuance');
   }
-}
+};
